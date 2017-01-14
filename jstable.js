40369f64c07aa735c832055d3f347f818b794c3c
Марк <markusx64bit@inbox.ru>
2017-01-15 01:00:11 +0800

    Ver. 2.0

--- jstable.js
+++ jstable.js
@@ -6,75 +6,10 @@
  * Released under the MIT license
  * https://github.com/Bencute/jstable/blob/master/LICENSE
  *
- * Version 1.0
+ * Version 2.0
  */
 
-jstable = {
-	create: function(idelem, rows, cols, options){
-		var options = options || {};
-		options = Object.assign({}, this.defaultOptions, options);
-		
-		var type = options.type;
-		var values = options.values;
-		
-		var container = document.getElementById(idelem);
-		var instance = new instanceTable(rows, cols);
-		
-		iOffset = (options.titleCols != null) ? 1 : 0 ;
-		gOffset = (options.titleRows != null) ? 1 : 0 ;
-		
-		var table = container.appendChild(document.createElement('table'));
-		instance.elemTable = table;
-		for (var i = 0 - iOffset; i < rows; i++){
-			var tr = table.appendChild(document.createElement('tr'));
-			for (var g = 0 - gOffset; g < cols; g++){
-				var td = tr.appendChild(document.createElement('td'));
-				if (i < 0 && g < 0) {
-					continue;
-				}
-				else if (i < 0 ^ g < 0) {	//XOR
-					if (i < 0){
-						var value = (typeof options.titleCols[g] != "undefined") ? options.titleCols[g] : '' ;
-					}
-					else if (g < 0) {
-						var value = (typeof options.titleRows[i] != "undefined") ? options.titleRows[i] : '' ;
-					}
-					td.textContent = value;
-				}
-				else {
-					var value = (typeof values[i] != "undefined" && typeof values[i][g] != "undefined") ? values[i][g] : '' ;
-					instance.change(i, g, value);
-					switch (type){
-						case 'input':
-							var input = td.appendChild(document.createElement('input'));
-							input.type = "text";
-							input.row = i;
-							input.col = g;
-							input.value = value;
-							input.tableInstance = instance;
-							input.addEventListener("change", function(){
-									this.tableInstance.change(this.row, this.col, this.value);
-							});
-							break;
-						case 'text':
-							td.textContent = value;
-							break;
-					}
-				}
-			}
-		}
-		return instance;
-	},
-	
-	defaultOptions: {
-		type: 'input',	// input | text
-		values: [],
-		titleCols: null,
-		titleRows: null,
-	}
-}
-
-function instanceTable(rows, cols){
+function JsTable(idelem, rows, cols, options){
 	this.data = [];
 	this.elemTable;
 	
@@ -84,16 +19,75 @@ function instanceTable(rows, cols){
 			this.data[i][g] = null;
 		}
 	}
+	
+	var options = options || {};
+	options = Object.assign({}, this.defaultOptions, options);
+	
+	var type = options.type;
+	var values = options.values;
+	
+	var container = document.getElementById(idelem);
+	
+	iOffset = (options.titleCols != null) ? 1 : 0 ;
+	gOffset = (options.titleRows != null) ? 1 : 0 ;
+	
+	var table = container.appendChild(document.createElement('table'));
+	this.elemTable = table;
+	for (var i = 0 - iOffset; i < rows; i++){
+		var tr = table.appendChild(document.createElement('tr'));
+		for (var g = 0 - gOffset; g < cols; g++){
+			var td = tr.appendChild(document.createElement('td'));
+			if (i < 0 && g < 0) {
+				continue;
+			}
+			else if (i < 0 ^ g < 0) {	//XOR
+				if (i < 0){
+					var value = (typeof options.titleCols[g] != "undefined") ? options.titleCols[g] : '' ;
+				}
+				else if (g < 0) {
+					var value = (typeof options.titleRows[i] != "undefined") ? options.titleRows[i] : '' ;
+				}
+				td.textContent = value;
+			}
+			else {
+				var value = (typeof values[i] != "undefined" && typeof values[i][g] != "undefined") ? values[i][g] : '' ;
+				this.data[i][g] = value;
+				switch (type){
+					case 'input':
+						var input = td.appendChild(document.createElement('input'));
+						input.type = "text";
+						input.row = i;
+						input.col = g;
+						input.value = value;
+						input.tableInstance = this;
+						input.addEventListener("change", function(){
+								this.tableInstance.change(this.row, this.col, this.value);
+						});
+						break;
+					case 'text':
+						td.textContent = value;
+						break;
+				}
+			}
+		}
+	}
 }
 
-instanceTable.prototype.change = function(row, col, value){
+JsTable.prototype.change = function(row, col, value){
 	this.data[row][col] = value;
 }
 
-instanceTable.prototype.getData = function(){
+JsTable.prototype.getData = function(){
 	return this.data;
 }
 
-instanceTable.prototype.delete = function(){
+JsTable.prototype.delete = function(){
 	this.elemTable.remove();
+}
+
+JsTable.prototype.defaultOptions = {
+	type: 'input',	// input | text
+	values: [],
+	titleCols: null,
+	titleRows: null,
 }
